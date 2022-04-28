d01b0e0ec3cc5338e94550b4e819cf0bf4f74a30
Inaiat <inaiat@gmail.com>
2022-04-28 18:18:30 -0300

    refactoring

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb'
-import { fromPromise, fromThrowable } from 'neverthrow'
+import { fromPromise, fromThrowable, ok } from 'neverthrow'
 import Papr from 'papr'
 import { userSchema } from '../user/user.model'
 import { exceptionHandler } from './error.handler'
@@ -8,17 +8,19 @@ export type MongoConfig = ReturnType<typeof defaultMongoConfig>
 
 const papr = new Papr()
 
-const safeInitialize = fromThrowable((client: MongoClient, dbName: string) => {
-  papr.initialize(client.db(dbName))
-  return client
-}, exceptionHandler)
+const safeInitialize = fromThrowable(
+  (client: MongoClient, dbName: string) => papr.initialize(client.db(dbName)),
+  exceptionHandler
+)
 
 export const defaultMongoConfig = async (dbUrl: string, dbName: string) =>
-  fromPromise(MongoClient.connect(dbUrl), exceptionHandler)
-    .andThen((client) => safeInitialize(client, dbName))
-    .map((client) => ({
-      papr,
-      connection: client,
-      userModel: papr.model('User', userSchema),
-      disconect: () => client.close(),
-    }))
+  fromPromise(MongoClient.connect(dbUrl), exceptionHandler).andThen((client) =>
+    safeInitialize(client, dbName)
+      .map(() => fromPromise(papr.updateSchemas(), exceptionHandler))
+      .map(() => ({
+        papr,
+        connection: client,
+        userModel: papr.model('User', userSchema),
+        disconect: () => client.close(),
+      }))
+  )
