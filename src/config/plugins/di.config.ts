1b806002cfa8c38e79617717b51c082142754af8
Inaiat <inaiat@gmail.com>
2022-05-01 20:37:32 -0300

    upgrade fastify-awilix-plug

--- src/config/plugins/di.config.ts
+++ src/config/plugins/di.config.ts
@@ -5,8 +5,8 @@ import { MongoClient } from 'mongodb'
 import { defaultUserRepository, UserCollection, UserRepository } from '../../user/user.repository'
 import { defaultUserServices, UserServices } from '../../user/user.service'
 import { FastifyPluginAsync } from 'fastify'
-import { fastifyAwilixPlugin } from '@inaiat/fastify-awilix-plugin'
 import { userModel } from './mongo.papr'
+import { FastifyAwilix } from '@inaiat/fastify-awilix-plugin'
 
 declare module '@inaiat/fastify-awilix-plugin' {
   interface Cradle {
@@ -20,16 +20,13 @@ declare module '@inaiat/fastify-awilix-plugin' {
 
 export default fp<FastifyPluginAsync>(
   async (fastify) => {
-    const env = appConfig()
-    fastify.register(fastifyAwilixPlugin, {
-      module: {
-        config: asValue(env),
-        connection: asValue(fastify.mongo),
-        userRepository: asFunction(defaultUserRepository).singleton(),
-        userCollection: asValue(userModel),
-        userServices: asFunction(defaultUserServices).singleton(),
-      },
-      injectionMode: 'CLASSIC',
+    const container = new FastifyAwilix(fastify, 'CLASSIC')
+    container.register({
+      config: asValue(appConfig()),
+      connection: asValue(fastify.mongo),
+      userRepository: asFunction(defaultUserRepository).singleton(),
+      userCollection: asValue(userModel),
+      userServices: asFunction(defaultUserServices).singleton(),
     })
   },
   { name: 'di.config' }
